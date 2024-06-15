9fb606a2cf9d5d225ccf2a858d7c5a4fcfa2b2e6
Anuoluwa Adebayo <adebayo.anuoluwa02@gmail.com>
2024-06-15 11:58:55 +0100

    fix: form input validation test

--- src/utils/__test__/validata.form.test.ts
+++ src/utils/__test__/validata.form.test.ts
@@ -1,4 +1,4 @@
-import { checkPattern } from "../validate.form";
+import { checkPattern, emailRegExp, passwordRegExp } from "../validate.form";
 
 describe("run basic validator test", () => {
   const mockRegEx = /^(?:.{3,})$/;
@@ -12,7 +12,39 @@ describe("run basic validator test", () => {
   test("check if text with character less than 3 is invalid", () => {
     const valid = checkPattern(mockRegEx, "an");
     console.log(valid);
-
     expect(valid).toBeFalsy();
+  });
+});
+
+describe("run test for email validation", () => {
+  const rightEmail = "adebayo.anuoluwa02@gmail.com";
+  const wrongEmail = "emailaddress@something";
+
+  test("should validate the right email address", () => {
+    const vaildate = checkPattern(emailRegExp, rightEmail);
+    expect(vaildate).toBeTruthy();
+  });
+
+  test("should not validate wrong email address", () => {
+    const validate = checkPattern(emailRegExp, wrongEmail);
+    expect(validate).toBeFalsy();
+  });
+});
+
+describe("run test for password validation", () => {
+  const rightPassword = "Pa$$w0rd?";
+  const wrongPassword1 = "Mystr0ngPassword";
+  const wrongPassword2 = "d0nuT?";
+
+  test("should validate the right password pattern", () => {
+    expect(checkPattern(passwordRegExp, rightPassword)).toBeTruthy();
+  });
+
+  test("should not validate the wrong password pattern (looks strong)", () => {
+    expect(checkPattern(passwordRegExp, wrongPassword1)).toBeFalsy();
+  });
+
+  test("should not validate the wrong password pattern (looks weak)", () => {
+    expect(checkPattern(passwordRegExp, wrongPassword2)).toBeFalsy();
   });
 });
--- src/utils/validate.form.ts
+++ src/utils/validate.form.ts
@@ -1,13 +1,29 @@
-export const emailRegExp = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,4}$/u; //  Basic Email Address Validation (Unicode Support)
+//  Remember to always include the global(g) flag :)
+export const emailRegExp: RegExp =
+  /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/g;
 
-export const passwordRegExp =
-  /^(?=.*[A-Z])(?=.*[a-z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$/; //  At least one uppercase letter, one lowercase letter, one number, and one special character
+export const passwordRegExp: RegExp =
+  /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/g;
 
 /*
-^(?=.*[A-Z])(?=.*[a-z])(?=.*\\d)[A-Za-z\\d]{8,}$    -   At least one uppercase letter, one lowercase letter, and one number (no specific special characters)
+Minimum eight characters, at least one letter and one number:
 
-^(?=.*[A-Z])(?=.*[a-z])(?=.*\\d)(?=.*[^\\da-zA-Z]).{8,}$    -   At least one uppercase letter, one lowercase letter, one number, and one special character (including symbols)
-  */
+"^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$"
+Minimum eight characters, at least one letter, one number and one special character:
+
+"^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$"
+Minimum eight characters, at least one uppercase letter, one lowercase letter and one number:
+
+"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$"
+Minimum eight characters, at least one uppercase letter, one lowercase letter, one number and one special character:
+
+"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$"
+Minimum eight and maximum 10 characters, at least one uppercase letter, one lowercase letter, one number and one special character:
+
+"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,10}$"
+*/
+
+//  PATTERN VALIDATOR
 
 /**
  *
@@ -16,6 +32,5 @@ export const passwordRegExp =
  * @returns
  */
 export const checkPattern = (pattern: RegExp, input: string): boolean => {
-  console.log(pattern)
-  return pattern.test(input) === true ? true : false;
+  return pattern.test(input);
 };
