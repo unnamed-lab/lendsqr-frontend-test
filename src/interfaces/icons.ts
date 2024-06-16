5a813f17a64c9bc9ef32eeba06c4a2c8d3ab270f
Anuoluwa Adebayo <adebayo.anuoluwa02@gmail.com>
2024-06-16 20:58:40 +0100

    feat: create icon components and interface

---
+++ src/interfaces/icons.ts
@@ -0,0 +1,13 @@
+export interface IconProps {
+  width?: number;
+  height?: number;
+  className?: string;
+  pathFill?: string;
+}
+
+
+export interface DashboardIconProps {
+  width?: number;
+  height?: number;
+  className?: string;
+}
